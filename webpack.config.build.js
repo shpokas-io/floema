969fee3b80c8c238942290441534971f9c14d2fd
shpokas-io <s.shpokas@gmail.com>
2026-02-03 19:49:14 +0200

    feat: finalize webpack setup

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,13 +1,12 @@
-const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path')
 
-const { merge } = require("webpack-merge");
-const config = require("./webpack.config.js");
+const { merge } = require('webpack-merge')
+const config = require('./webpack.config')
 
 module.exports = merge(config, {
-  mode: "production",
+  mode: 'production',
+
   output: {
-    path: path.join(__dirname, "public"),
-  },
-  plugins: [new CleanWebpackPlugin()],
-});
+    path: path.join(__dirname, 'public')
+  }
+})
